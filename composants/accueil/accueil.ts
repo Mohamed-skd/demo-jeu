0345fa63efb9c568433c60b4df4d2ddb5341d208
Mohamed <contactdiabysn@gmail.com>
2022-12-06 00:00:37 +0100

    devinette ok

--- composants/accueil/accueil.ts
+++ composants/accueil/accueil.ts
@@ -25,11 +25,19 @@ export default function accueil() {
   const devInfo = select("#dev-info")!;
   const devChoix = select("#dev-choix") as HTMLInputElement;
   const devHasard: number = hasard(101);
-  let info: string =
-    "Vous devez deviner un nombre entre 0 et 100, bonne chance.";
+
+  let chances: number = 10;
+  let info: string = `Vous devez deviner un nombre entre 0 et 100, Vous avez droit à ${chances} tentatives.`;
 
   function devInfos() {
     devInfo.textContent = info;
+    devChoix.value = "";
+  }
+  function devReset() {
+    devChoix.disabled = true;
+    setTimeout(() => {
+      location.assign("");
+    }, 1000);
   }
 
   devInfos();
@@ -39,16 +47,27 @@ export default function accueil() {
 
     if (isNaN(choix) || choix < 0 || choix > 100) {
       info = "Vous devez choisir un nombre entre 0 et 100 👀";
+      devInfos();
+      return;
     }
 
     if (choix === devHasard) {
-      info = `Bravo ! Vous avez trouvé ${devHasard} !`;
+      info = `Bravo 😊 ! Vous avez trouvé ${devHasard}.`;
+      devReset();
+    } else if (choix !== devHasard && chances === 1) {
+      info = `Dommage 😢 ! Il fallait trouver ${devHasard}.`;
+      devReset();
     } else if (choix < devHasard) {
-      info = `${choix} est trop petit.`;
+      chances--;
+      info = `${choix} est trop petit. Il vous reste ${chances} tentatives ✊`;
     } else if (choix > devHasard) {
-      info = `${choix} est trop grand.`;
+      chances--;
+      info = `${choix} est trop grand. Il vous reste ${chances} tentatives ✊`;
     }
 
     devInfos();
+    devChoix.focus();
   });
+
+  // shifumi
 }
